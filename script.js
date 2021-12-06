5285b37636a4d12a7b42b2790170ea204bea6d2d
Alan Do <amdworkspace@gmail.com>
2021-12-05 21:39:39 -0500

    functioning JS to generate password

--- script.js
+++ script.js
@@ -15,58 +15,60 @@ function generatePassword() {
 // Step 1. Prompt the user for the password criteria.
 //  a. Password length 8 < 128
 
-  var passwordLengthUser = parseInt(prompt("Password must be between 8-128 characters."));
+  var passwordLengthUser = 0
  
 
+while (passwordLengthUser < 8 || passwordLengthUser > 128) {
+  passwordLengthUser = parseInt(prompt("How many characters would you like your password to contain? Password must be between 8-128 characters."));
   if (passwordLengthUser < 8) {
     alert("Password must have more than 7 characters!");
-    return "Your password doesn't contain more than 7 characters!";
   }
-
   if (passwordLengthUser > 128) {
-    alert("Password must not have more than 128 characters!");
-    return "Your password exceeds the character limit of 128 characters!";
+    alert("Password must not have more than 128 characters!")
   }
+}
+var lowercaseCharactersChoice = false;
+var uppercaseCharactersChoice = false;
+var numericalCharactersChoice = false;
+var specialCharactersChoice = false;
 
 //  b. Lowercase, Uppercase, Numbers, Special Characters
-
-  var lowercaseCharactersChoice = confirm("Would you like your password to contain lowercase characters?");
+while((lowercaseCharactersChoice || uppercaseCharactersChoice || numericalCharactersChoice || specialCharactersChoice) == false) {
+  lowercaseCharactersChoice = confirm("Would you like your password to contain lowercase characters?");
+  uppercaseCharactersChoice = confirm("Would you like your password to contain numerical characters?");
+  numericalCharactersChoice = confirm("Would you like your password to contain uppercase characters?");
+  specialCharactersChoice = confirm("Would you like your password to contain special characters?");
+  if ((lowercaseCharactersChoice || uppercaseCharactersChoice || numericalCharactersChoice || specialCharactersChoice) == false) {
+    alert("You must select one of the character choices!");
+  }
+}
+// Step 2. Validate the input.
+var charset = "";
 
   if (lowercaseCharactersChoice) {
-    passwordChar += lowercaseChar;
+    charset += lowercaseChar;
   }
-
-  var uppercaseCharactersChoice = confirm("Would you like your password to contain uppercase characters?");
-
   if (uppercaseCharactersChoice) {
-    passwordChar += uppercaseChar;
+    charset += uppercaseChar;
   }
-
-  var numericalCharactersChoice = confirm("Would you like your password to contain numerical characters?");
-
   if (numericalCharactersChoice) {
-    passwordChar += numericalChar;
+    charset += numericalChar;
   }
-
-  var specialCharactersChoice = confirm("Would you like your password to contain special characters?");
-
   if (specialCharactersChoice) {
-    passwordChar += specialChar;
+    charset += specialChar;
   }
-
-  if (!lowercaseCharactersChoice && !uppercaseCharactersChoice && !numericalCharactersChoice && !specialCharactersChoice) {
-    alert("Your password must contain at least one special, numeric, lowercase, or uppercase character");
-    return;
-  }
-
-  for (var i = 0; i < passwordLengthUser; i++) {
-    password = passwordChar[Math.floor(Math.random() * passwordChar.length)]
-  }
-
-  
-// Step 2. Validate the input.
+var charsetLength = charset.length;
+var counter = 0;
+var password = "";
 
 // Step 3. Generate password based on criteria.
+while(counter < passwordLengthUser) {
+  var randomNum = Math.floor(Math.random() * charsetLength);
+  password += charset.charAt(randomNum);
+  counter += 1
+}
+alert("The random password generated is: " + password)
+
 // Step 4. Display the generated password to the page.
 return "Generated password will go here!";
 
